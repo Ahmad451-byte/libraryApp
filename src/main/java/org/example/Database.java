96e2448b3223402eaa8a06b8bccf182d89c998cb
Ahmad451-byte <ahmaa19920105@Hotmail.com>
2025-05-23 16:31:08 +0200

    Update Database.java

--- src/main/java/org/example/Database.java
+++ src/main/java/org/example/Database.java
@@ -3,9 +3,9 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 public class Database {
-    private static final String URL = "jdbc:mysql://localhost:3306/library";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String URL = "jdbc:mysql://<HOST><PORT>/<DB_NAME>";
+    private static final String USER =" <USERNAME>";
+    private static final String PASSWORD = "<PASSWORD>";
 
     public static void createTable() {
         try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
